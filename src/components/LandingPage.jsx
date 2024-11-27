588909265d8bf6ea06ce3a1a0b7f018e15353b9d
Abdul <hafizhahm123@gmail.com>
2024-11-27 10:02:33 +0700

    init landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,19 @@
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="background-image">
+        <img src="/garden.jpg" alt="garden" />
+      </div>
+      <div className="content">
+        <div className="landing_content">
+          <h1>Welcome to Unique Plants!</h1>
+        </div>
+        <div className="divider"></div>
+        <p>Great Nature, Great Responsibility!</p>
+        <div className="aboutus_container"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
