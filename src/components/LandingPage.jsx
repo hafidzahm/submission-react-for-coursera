749a15cfe16e23968339eb07ca09f92e4f0c5580
Abdul <hafizhahm123@gmail.com>
2024-11-27 10:06:47 +0700

    init get started button

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,9 +7,11 @@ const LandingPage = () => {
       <div className="content">
         <div className="landing_content">
           <h1>Welcome to Unique Plants!</h1>
+          <div className="divider"></div>
+          <p>Great Nature, Great Responsibility!</p>
+          <button className="get-started-button">Get Started</button>
         </div>
-        <div className="divider"></div>
-        <p>Great Nature, Great Responsibility!</p>
+
         <div className="aboutus_container"></div>
       </div>
     </div>
