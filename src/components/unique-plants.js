baf151ea59fb0b96d2e6fb3ec9fd5b3ecc36fda1
Abdul <hafizhahm123@gmail.com>
2024-11-27 11:50:12 +0700

    adding plants

---
+++ src/components/unique-plants.js
@@ -0,0 +1,67 @@
+const plants = [
+  {
+    category: 'Easy Grow Plants',
+    plants: [
+      {
+        name: 'Frizzle Sizzle (Albuca spiralis)',
+        img: './frizzle-sizzle.jpg',
+        description:
+          'The Frizzle Sizzle is a delightful plant that has a fun and cool name. It also looks like something out of an animated cartoon because of its green corkscrew appearance. Admired for its sculptural, almost architectural look, the Albuca features spiraling leaves that are thick and curly.',
+        price: 25,
+      },
+      {
+        name: 'Hoya Lucky Heart (Hoya Kerri)',
+        img: './hoya.jpg',
+        description:
+          'We could not leave out this heart-shaped succulent that would please anyone if received. The Hoya Lucky Heart features super cool, heart-shaped leaves. In fact, they are often sold as a single heart leaf in planters.',
+        price: 25,
+      },
+    ],
+  },
+  {
+    category: 'Expensive Plants',
+    plants: [
+      {
+        name: 'Air Plant (Tillandsia xerographica)',
+        img: '/air-plant.jpg',
+        description:
+          'Even as air plants increase in popularity, the Xerographica variety remains rare. Greenish silvery leaves in the form of a rosette that curl inward underneath give it an almost ghostly appearance. Air plants are slow growers, but the cool thing about these epiphytic plants is that they do not require soil to survive or thrive. You can cultivate your tillandsia by perching it on a piece of wood or stone, or by simply placing it in a colorful bowIf you live in a humid climate, misting your air plant should be sufficient. If not, soak it in water once a week. Remove excess water by allowing the water to drip off the plant when held upside down.',
+        price: 25,
+      },
+      {
+        name: 'Banana Shrub (Magnolia Figo)',
+        img: './banana-shrub.jpg',
+        description:
+          'The banana shrub is not related to the banana we eat, but the creamy yellow blossoms with a purple tinge do smell like them.',
+        price: 25,
+      },
+      {
+        name: 'Black Bat Flower (Tacca chantrieri)',
+        img: '/black-bat.jpg',
+        description:
+          'This is one unusual plant that will undoubtedly attract attention. The Black Bat Flower distinguishes itself from the pack by producing black flowers that some think resemble a bat, while others say its a cat with whiskers.',
+        price: 25,
+      },
+    ],
+  },
+  {
+    category: 'Powerful Plants',
+    plants: [
+      {
+        name: 'Desert Rose (Adenium obesum)',
+        img: '/dessert-rose.jpg',
+        description:
+          'Native to Eastern Africa, the Desert Rose is a lovely flowering plant that loves a sunny, warm environment in which to produce its lovely bright red or pink blooms.',
+        price: 25,
+      },
+      {
+        name: 'Echinopsis (Echinopsis calochora)',
+        img: './echinopsis.jpg',
+        description:
+          'The Echinopsis is a great plant for the night owls among us. A cactus, it appears to be a typical smallish mound with thorny ribs.',
+        price: 25,
+      },
+    ],
+  },
+];
+export default plants;
